d698e5d321fc14f159f82c0b4a36de900c14eccd
Elliot <elliot.james.williams@gmail.com>
2018-01-03 01:50:00 -0500

    Added sim code for riddler simulation (should also probably practice d3 with this lol

--- RiddlerClassicHoliday17.py
+++ RiddlerClassicHoliday17.py
@@ -4,8 +4,6 @@ def leftRightCenterSim(playernum, moneynum):
     assert playernum > 0 and isinstance(playernum, int)
     assert moneynum  > 0 and isinstance(moneynum, int)
 
-    print("Simulation w {0} players for ${1}".format(playernum, moneynum))
-
     moneyarray = [moneynum] * playernum
     num_iter = 1
     while sum(moneyarray) != 0:
@@ -26,4 +24,31 @@ def leftRightCenterSim(playernum, moneynum):
     print(moneyarray)
     print(num_iter)
 
-leftRightCenterSim(3,1)
+    return num_iter
+
+def lrcTrial(numtrials, playernum, moneynum):
+
+    cum_squared_num_iter = 0
+    cum_num_iter = 0
+    for i in range(numtrials):
+        curr_result = leftRightCenterSim(playernum, moneynum)
+        cum_num_iter += curr_result
+        cum_squared_num_iter += curr_result ** 2.
+
+    expected_num_iter = float(cum_num_iter) / numtrials
+    expected_squared_num_iter = float(cum_squared_num_iter) / numtrials
+
+    stddev = (expected_squared_num_iter - (expected_num_iter ** 2.)) ** (1/2.)
+
+    return [expected_num_iter, stddev]
+
+
+if __name__ == "__main__":
+
+    numtrials = 1000
+    playernum = 6
+    moneynum  = 3
+
+    print("Simulation w {0} players for ${1}".format(playernum, moneynum))
+    results = lrcTrial(numtrials, playernum, moneynum )
+    print("Average is {0}, StdDev is {1}".format(results[0], results[1]))
