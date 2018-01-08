52a6bded8a284b7661285f67ab37ce209c78351e
Elliot <elliot.james.williams@gmail.com>
2018-01-08 17:26:07 -0500

    Added iterative dwarf simulation code. To come: data collection

--- DwarfSleeping.py
+++ DwarfSleeping.py
@@ -20,45 +20,69 @@ import random
 Simulation function for one round of this game of dwarven musical beds.
 '''
 
-def DwarfSimulation(numDwarves=7):
+def dwarfSimulation(num_dwarves=7, verbose=False):
     # Initializes a list of dwarves.
-    # Dwarves are identified by their numbers (1, ..., numDwarves)
-    dwarfList    = [x+1 for x in range(numDwarves)]
+    # Dwarves are identified by their numbers (1, ..., num_dwarves)
+    dwarfList    = [x+1 for x in range(num_dwarves)]
     # w.l.o.g. say that each dwarf 1 owns bed 0, ..., dwarf k owns bed k-1
     # initially, all beds are empty
-    emptyBedList = [x for x in range(numDwarves)]
-    bedList      = ["Empty"] * numDwarves
+    emptyBedList = [x for x in range(num_dwarves)]
+    bedList      = ["Empty"] * num_dwarves
 
     # First Dwarf Chooses Randomly a Bed that Isn't His Own
     rand_bed = random.sample(emptyBedList[1:], k=1)[0]
     bedList[rand_bed] = "Dwarf{}".format(1)
 
+    # Here we keep track, using a set, all remaining empty beds
     emptyBedSet = set(emptyBedList)
     emptyBedSet.remove(rand_bed)
 
-    print('Dwarves: {}'.format(dwarfList))
-    print('Beds: {}'.format(bedList))
-    print('EmptyBedSet: {}'.format(emptyBedSet))
-
     # Iterates over remaining dwarves
     for dwarf_num in dwarfList[1:]:
-        print(dwarf_num)
         bed_num = dwarf_num - 1
+        # If a dwarf's bed is empty, he sleeps in it
         if bedList[bed_num] == "Empty":
             bedList[bed_num] = "Dwarf{}".format(dwarf_num)
             emptyBedSet.remove(bed_num)
+        # Otherwise, he picks a bed at random to sleep in
         else:
             rand_bed = random.sample(emptyBedSet, k=1)[0]
             bedList[rand_bed] = "Dwarf{}".format(dwarf_num)
             emptyBedSet.remove(rand_bed)
 
-    print('Dwarves: {}'.format(dwarfList))
-    print('Beds: {}'.format(bedList))
-    print('EmptyBedSet: {}'.format(emptyBedSet))
-
     lastDwarf = dwarfList[-1]
-    for bedIndex in range(len(bedList)):
-        if bedList[bedIndex] == "Dwarf{}".format(lastDwarf):
-            print("Dwarf{} is in Dwarf{}'s bed".format(lastDwarf, bedIndex+1))
+    LastDwarfInOwnBed = (bedList[-1] == "Dwarf{}".format(lastDwarf))
 
-DwarfSimulation(7)
+    num_displaced_dwarves = 0
+
+    for bedIndex in range(len(bedList)):
+        if "Dwarf{}".format(bedIndex+1) != bedList[bedIndex]:
+            num_displaced_dwarves += 1
+    if verbose:
+        print('Dwarves: {}'.format(dwarfList))
+        print('Beds: {}'.format(bedList))
+        print('# Displaced Dwarves: {}'.format(num_displaced_dwarves))
+
+'''
+Function for iterating dwarfSimulation many times
+Input:
+    num_iter    : int  (# of times you want to simulate dwarf beds)
+    num_dwarves : int  (# dwarves simulated)
+    verbose     : bool (whether of not you want to print out all details)
+    SEED        : int  (the seed of the simulation)
+
+Output:
+    probLastDwarfInOwnBed : float (the probability of last dwarf in his bed)
+    expectedNumDisplaced  : float (the expected number of bed-displaced dwarves)
+'''
+def iterateDwarfSim(num_iter, num_dwarves=7, verbose=False, SEED=1):
+    print("Running Many Dwarven Bed Simulations w {} dwarves, {} iterations".format(
+        num_dwarves, num_iter
+    ))
+    random.seed(SEED)
+    for x in range(num_iter):
+        if x % 1000 == 0:
+            print("Running Dwarf Sim {}".format(x))
+        dwarfSimulation(num_dwarves, verbose=verbose)
+
+iterateDwarfSim(num_iter=10000, num_dwarves=7, verbose=False)
