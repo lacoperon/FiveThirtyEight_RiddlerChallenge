a0cc68ebd2597aca5ebcfcde020f2c54b2e3919f
Elliot <elliot.james.williams@gmail.com>
2018-01-08 12:23:04 -0500

    Added initial .py file for the dwarven simulation (they're very upsetting)

---
+++ DwarfSleeping.py
@@ -0,0 +1,15 @@
+'''
+Riddler Prompt for Jan 5, 2018
+
+Each of the seven dwarfs sleeps in his own bed in a shared dormitory.
+Every night, they retire to bed one at a time, always in the same sequential
+order, with the youngest dwarf retiring first and the oldest retiring last.
+On a particular evening, the youngest dwarf is in a jolly mood. He decides not
+to go to his own bed but rather to choose one at random from among the other
+six beds. As each of the other dwarfs retires, he chooses his own bed if it
+isn't occupied, and otherwise chooses another unoccupied bed at random.
+
+1. What is the probability that the oldest dwarf sleeps in his own bed?
+
+2. What is the expected number of dwarfs who do not sleep in their own beds?
+'''
